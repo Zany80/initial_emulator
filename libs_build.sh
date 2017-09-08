314b6a188e977e49906231f3db2aa202b4713386
pixelherodev <npreil@pleasanta.tk>
2017-09-08 13:01:30 +0000

    Bitbucket pipelines config

---
+++ libs_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+pushd $HOME
+if [ ! -d "./SFML" ]
+then
+	git clone https://github.com/SFML/SFML
+else
+	pushd SFML
+	git pull
+	popd
+fi
+cd SFML
+mkdir -p build
+cd build
+cmake ..
+make -j6
+make install
+cd $HOME
+if [ ! -d "./TGUI" ]
+then
+	git clone https://github.com/texus/TGUI
+else
+	pushd TGUI
+	git pull
+	popd
+fi
+cd TGUI
+mkdir -p build
+cd build
+cmake ..
+make -j6
+make install
+popd

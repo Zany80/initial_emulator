Zany80/initial_emulator

commit 1af27018659f971e1f2ce47d63a13f320fdb49c2

Detect cart removal

src/zenith80.cpp
@@ -129,21 +129,44 @@ Main::Main(int argc,char ** argv){
 	if(!name_changed){
 		cout<<"No --file argument received, defaulting to \"zenith.bin\"..."<<endl;
 	}
-	if (waiting) {
-		canvas->clear();
-		sf::Text insert("Insert cartridge...",*default_font,18);
-		insert.setPosition(400-insert.getGlobalBounds().width/2,300-insert.getGlobalBounds().height/2);
-		canvas->draw(insert);
-		while(!exists(name.c_str())){
-			update();
+	bool done=false;
+	RAMController *r=0;
+	DeviceController *d=0;
+	while (!done) {
+		try {
+			if(waiting) {
+				wait(name.c_str());
+			}
+			r=new RAMController(name.c_str());
+			cpu=new Z80(new RAMController(name.c_str()),new DeviceController());
+			done=true;
+		}
+		catch(int i) {
+			if(r){
+				delete r;
+				r=0;
+			}
+			if(d){
+				delete d;
+				d=0;
+			}
 		}
 	}
-	cpu=new Z80(new RAMController(name.c_str()),new DeviceController());
 	gpu=new GR80(canvas,cpu);
 	putmsg(((string)"Git revision: ")+STRINGIFY(GIT_REVISION));
 	termOut->addLine("");
 	termOut->addLine("");
 	cpu->reset();
+}
+
+void Main::wait(const char *name){
+	canvas->clear();
+	sf::Text insert("Insert cartridge...",*default_font,18);
+	insert.setPosition(400-insert.getGlobalBounds().width/2,300-insert.getGlobalBounds().height/2);
+	canvas->draw(insert);
+	while(!exists(name)){
+		update();
+	}
 }
 
 Main::~Main(){

include/main.hpp
@@ -61,6 +61,7 @@ class Main{
 	private:
 		Clock accuracy_clock;
 		Clock precision_clock;
+		void wait(const char *name);
 		vector<uint8_t> key_buffer;
 		uint64_t unit;
 		uint64_t clock_speed;

src/CPU/peripherals/RAMControllerNew.cpp
@@ -119,6 +119,9 @@ RAMController::RAMController(const char * name){
 						sf::sleep(sf::seconds(3));
 						exit(0);
 					}
+					if(!exists(name)) {
+						throw 3;
+					}
 				}
 				Main::instance->resetKeyBuffer();
 				int length=(int)size-(int)start;

commit a3de6871a3e66d57d727f6f15b82eb5f9149235e

Fix a bug introduced by 1af2701

src/zenith80.cpp
@@ -138,7 +138,8 @@ Main::Main(int argc,char ** argv){
 				wait(name.c_str());
 			}
 			r=new RAMController(name.c_str());
-			cpu=new Z80(new RAMController(name.c_str()),new DeviceController());
+			d=new DeviceController();
+			cpu=new Z80(r,d);
 			done=true;
 		}
 		catch(int i) {
@@ -165,7 +166,8 @@ void Main::wait(const char *name){
 	insert.setPosition(400-insert.getGlobalBounds().width/2,300-insert.getGlobalBounds().height/2);
 	canvas->draw(insert);
 	while(!exists(name)){
-		update();
+		////update();
+		sf::sleep(sf::seconds(0.3));
 	}
 }
 